5839811ae1bb4e2e56473dcf675a52d4a080223c
Billie Alice Thompson <billie@billiecodes.com>
2013-09-09 19:19:51 +0100

    Made the tests work again

--- tests/_autoload.php
+++ tests/_autoload.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__ . '/../../zf2/library/Zend/Loader/AutoloaderFactory.php';
+require_once __DIR__ . '/../vendor/zendframework/zendframework/library/Zend/Loader/AutoloaderFactory.php';
 Zend\Loader\AutoloaderFactory::factory(array(
     'Zend\Loader\StandardAutoloader' => array(
         'autoregister_zf' => true,
